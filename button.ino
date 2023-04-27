a29a1f8660108ede33b356374be8d7700727830e
1204-West-Stephen <89425205+1204-West-Stephen@users.noreply.github.com>
2023-04-27 16:00:17 -0700

    Button
    thus is button

---
+++ button.ino
@@ -0,0 +1,106 @@
+//Code for the Button and Interupt
+
+#define RDA 0x80
+#define TBE 0x20
+
+//Pointer Initializations:
+volatile unsigned char *myUCSR0A = (unsigned char *)0x00C0;
+volatile unsigned char *myUCSR0B = (unsigned char *)0x00C1;
+volatile unsigned char *myUCSR0C = (unsigned char *)0x00C2;
+volatile unsigned int  *myUBRR0  = (unsigned int *) 0x00C4;
+volatile unsigned char *myUDR0   = (unsigned char *)0x00C6;
+
+int buttonState = 0; 
+
+int lastButtonState = 0;
+
+int buttonPushCounter = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2, INPUT);
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // read the pushbutton input pin
+  buttonState = digitalRead(2);
+  // compare the buttonState to its previous state
+  if (buttonState != lastButtonState) {
+    // if the state has changed, increment the counter
+    if (buttonState == HIGH) {
+      // if the current state is HIGH, then the button
+      // went from off to on
+      buttonPushCounter += 1;
+      Serial.println("on");
+      Serial.print("number of button pushes: ");
+      Serial.println(buttonPushCounter);
+    } else {
+      // if the current state is LOW, then the button
+      // went from on to off
+      Serial.println("off");
+    }
+    // delay a little bit to avoid debouncing
+    delay(5); // Wait for 5 millisecond(s)
+  }
+  // save the current state as the last state, for
+  // the next time through the loop
+  lastButtonState = buttonState;
+  // turns on the LED every four button pushes by
+  // checking the modulo of the button push counter.
+  // the modulo function gives you the remainder of
+  // the devision of two numbers
+  if (buttonPushCounter % 4 == 0) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+}
+
+void U0init(unsigned long U0baud)
+{
+
+ unsigned long FCPU = 16000000;
+ unsigned int tbaud;
+ tbaud = (FCPU / 16 / U0baud - 1);
+ // Same as (FCPU / (16 * U0baud)) - 1;
+ *myUCSR0A = 0x20;
+ *myUCSR0B = 0x18;
+ *myUCSR0C = 0x06;
+ *myUBRR0  = tbaud;
+}
+
+// Read USART0 RDA status bit and return non-zero true if set
+
+unsigned char U0kbhit()
+{
+  if(*myUCSR0A & RDA){
+    return true;
+  }
+  else{
+    return false;
+  }
+
+
+// Read input character from USART0 input buffer
+}
+unsigned char U0getchar()
+{
+  unsigned char ch;
+  while(!(UCSR0A & (1<<RXC0)));
+  ch =  *myUDR0;
+  return ch;
+  
+  
+}
+
+// Wait for USART0 TBE to be set then write character to transmit buffer
+void U0putchar(unsigned char U0pdata)
+{
+
+while((*myUCSR0A & (TBE)) == 0); 
+*myUDR0 = U0pdata;
+
+}
